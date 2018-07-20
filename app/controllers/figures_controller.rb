7de77b858bd5068a4a2054322779132a0ab4043a
Marissa Zhong <mwz2101@columbia.edu>
2018-07-20 20:05:51 +0000

    Done.

---
+++ app/controllers/figures_controller.rb
@@ -0,0 +1,50 @@
+class FiguresController < ApplicationController
+
+  get '/figures' do
+    @figures = Figure.all
+    erb :'/figures/index'
+  end
+
+  get '/figures/new' do
+    erb :'/figures/new'
+  end
+
+  get '/figures/:id' do
+    @figure = Figure.find(params[:id])
+    erb :'/figures/show'
+  end
+
+  post '/figures' do
+    @figure = Figure.create(params["figure"])
+    if !params[:title].empty?
+      @figure.titles << Title.create(params[:title])
+    end
+    if !params[:landmark].empty?
+      @figure.landmarks << Landmark.create(params[:landmark])
+    end
+    @figure.save
+    redirect to "/figures/#{@figure.id}"
+  end
+
+  get '/figures/:id/edit' do
+    @figure = Figure.find(params[:id])
+    erb :'/figures/edit'
+  end
+
+  post '/figures/:id' do
+    @figure = Figure.find(params[:id])
+    @figure.update(params[:figure])
+    # @figure.name = params[:figure][:name]
+    # @figure.title_ids = params[:figure][:title_ids]
+    # @figure.landmark_ids = params[:figure][:landmark_ids]
+    if !params[:title].empty?
+      @figure.titles << Title.create(params[:title])
+    end
+    if !params[:landmark][:name].empty?
+      @figure.landmarks << Landmark.create(params[:landmark])
+    end
+    @figure.save
+    redirect to "/figures/#{@figure.id}"
+  end
+
+end
